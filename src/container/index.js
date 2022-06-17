bae5d04c88ba5a277d977af0e92902d34d9fbc1a
Jesús Yelsin Broly <yelgari13@gmail.com>
2022-06-16 23:52:58 -0500

    Change invoker settign in container.

--- src/container/index.js
+++ src/container/index.js
@@ -1,24 +1,18 @@
 import { createContainer, asValue, asClass, asFunction, Lifetime } from 'awilix';
+
+//setting
 import configs from '../configs';
+import server from '../container/server';
+import routes from '../routes';
 
 const container = createContainer();
-container.register({ configs: asValue(configs) });
+container.register({
+  configs: asValue(configs),
+  server: asClass(server).singleton(),
+  routes: asFunction(routes).singleton(),
+});
 container.loadModules(
   [
-    [
-      'src/container/server.js',
-      {
-        register: asClass,
-        lifetime: Lifetime.SINGLETON,
-      },
-    ],
-    [
-      'src/routes/routes.js',
-      {
-        register: asFunction,
-        lifetime: Lifetime.SINGLETON,
-      },
-    ],
     [
       'src/handlers/**/*.handler.js',
       {
